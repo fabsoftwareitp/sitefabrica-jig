f494738477ac7fbc761292dbfe906578118cc99e
danilocbueno <danilocbueno@gmail.com>
2021-05-13 14:28:16 -0300

    Adicionado arquivo de reset CSS

--- source/_layouts/main.blade.php
+++ source/_layouts/main.blade.php
@@ -6,10 +6,20 @@
         <link rel="canonical" href="{{ $page->getUrl() }}">
         <meta name="description" content="{{ $page->description }}">
         <title>{{ $page->title }}</title>
+        
         <link rel="stylesheet" href="{{ trim($page->baseUrl, '/') }}{{ mix('css/main.css', 'assets/build') }}">
+        
         <script defer src="{{ trim($page->baseUrl, '/') }}{{ mix('js/main.js', 'assets/build') }}"></script>
     </head>
     <body class="text-gray-900 font-sans antialiased">
+
+        <nav class="navbar">
+            <ul>
+                <li>Projetos</li>
+                <li>Membros</li>
+            </ul>
+        </nav>
+
         @yield('body')
     </body>
 </html>
